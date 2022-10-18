4e7c8156a1040c59d85fd2eea372b18638e87ea1
Tereza Tylerová <Tereza.Tylerova@seznam.cz>
2022-10-18 21:43:44 +0200

    complete without function render

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('funguju!');
+const Task = (props) => {
+    let { name, due } = props;
+
+    return (`
+    <div class="task">
+        <div class="task__body">
+          <div class="task__name">${name}</div>
+          <div class="task__due">${due}</div>
+        </div>
+        <div class="task__done">✓</div>
+      </div>
+    `)
+}
+
+fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let element = document.querySelector(".todo__tasks");
+        element.innerHTML = data
+            .map((task) => Task(task))
+            .join("");
+    });
+
+
+let isChecked = true;
+const getUndo = () => {
+    if (isChecked === true) {
+        fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                let element = document.querySelector(".todo__tasks");
+                element.innerHTML = data
+                    .filter((task) => task.done === false)
+                    .map((task) => Task(task))
+                    .join("");
+            });
+        isChecked = false;
+    } else {
+        fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                let element = document.querySelector(".todo__tasks");
+                element.innerHTML = data
+                    .map((task) => Task(task))
+                    .join("");
+            });
+        isChecked = true;
+    }
+
+}
+
+document.getElementById("checkbox-undone").addEventListener("click", getUndo)
